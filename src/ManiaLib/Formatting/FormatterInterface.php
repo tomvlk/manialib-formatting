eea4c463a84635f8972ac237680a21438a2545bb
philippe <philippe@nadeo.com>
2014-09-05 11:49:25 +0200

    Changed phpDoc language

--- src/ManiaLib/Formatting/FormatterInterface.php
+++ src/ManiaLib/Formatting/FormatterInterface.php
@@ -8,82 +8,83 @@ interface FormatterInterface
     public function __construct($input);
 
     /**
-     * Initialise la chaine à transformer
+     * Init string to edit
      * @param string $input
-     * @return \static
+     * @return static
      */
     public function setInput($input);
 
     /**
-     * Retourne la chaine original
+     * Return original string
      * @return string
      */
     public function getInput();
 
     /**
-     * Supprime les codes fournis de la chaine
+     * Remove codes provided from input string
      * @param string[] $code
-     * @return \static
+     * @return static
      */
     public function stripCodes(array $code);
 
     /**
-     * Supprime tout les styles de la chaine
-     * @return \static
+     * Remove every styles from input string
+     * @return static
      */
     public function stripStyles();
 
     /**
-     * Supprime tout les liens de la chaine
-     * @return \static
+     * Remove every links from input string
+     * @return static
      */
     public function stripLinks();
 
     /**
-     * Supprime tout les codes liés aux couleurs de la chaine
-     * @return \static
+     * Remove every colors code from input string
+     * Remove
+     * @return static
      */
     public function stripColors();
 
     /**
-     * Supprime les formatages relatifs à la largeur des caractères dans la chaine
-     * @return \static
+     * Remove formating linked to character width from input string
+     * @return static
      */
     public function stripWideFonts();
 
     /**
-     * Modifie les couleurs de la chaine pour améliorer le contraste par rapport à la couleur fournie
+     * Change string colors to increase contrast with backgroundColor
      * @param string $backgroundColor chaine au format RGB12 ou RGB24, i.e FFF ou FFFFFF
-     * @return \static
+     * @return static
      */
-    public function contrastColots($backgroundColor);
+    public function contrastColors($backgroundColor);
 
     /**
-     * Retourne la chaine modifiée
+     * Return the modified string
      * @return string
      */
     public function toString();
 
 
     /**
-     * Retourne la chaine modifiée au format HTML
+     * Return the modified string in HTML
      * @return string
      */
     public function toHTML();
 
     /**
-     * Retourne la chaine modifiée
+     * Return the modified string
      * @return string
      */
     public function __toString();
 
     /**
-     * Incruste la chaine modifiée sur l'image fournie
-     * @param ressource $image Ressource d'image créée avec gd2
-     * @param string $fontName Chemin vers la police TTF
+     * Draw a string with on an image, method will be choose depending on the size wanted
+     * @param imageResource $image The image to draw on
+     * @param string $fontName The TTF font to use, which has been declared previously
      * @param int $x position
      * @param int $y position
-     * @param int $size taille de la police
+     * @param int $size Font size
      * @param type $defaultColor Default text color in 3 or 6 hexadecimal characters
      * @return void
      */
