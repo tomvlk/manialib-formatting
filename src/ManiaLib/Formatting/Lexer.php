af263632cfe35a4d66170ce150b6e9935e49fa97
philippe <philippe@nadeo.com>
2014-10-21 16:32:27 +0200

    Added regex patterns

--- src/ManiaLib/Formatting/Lexer.php
+++ src/ManiaLib/Formatting/Lexer.php
@@ -6,12 +6,17 @@ class Lexer extends \Doctrine\Common\Lexer\AbstractLexer
 {
     protected function getCatchablePatterns()
     {
-
+        return array(
+            '\$[hlp](\[[^\]]*\])',
+            '\$[0-9a-f][^\$]{0,2}',
+            '\$.',
+            '[^\$]*'
+        );
     }
 
     protected function getNonCatchablePatterns()
     {
-        
+        return array();
     }
 
     protected function getType(&$value)
