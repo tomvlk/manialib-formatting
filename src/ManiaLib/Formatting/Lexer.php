005992044a28af2866c30ae9a2c07427497a81f4
philippe <philippe@nadeo.com>
2014-10-21 16:27:00 +0200

    Created structure of Maniaplanet string lexer

---
+++ src/ManiaLib/Formatting/Lexer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ManiaLib\Formatting;
+
+class Lexer extends \Doctrine\Common\Lexer\AbstractLexer
+{
+    protected function getCatchablePatterns()
+    {
+
+    }
+
+    protected function getNonCatchablePatterns()
+    {
+        
+    }
+
+    protected function getType(&$value)
+    {
+
+    }
+}
