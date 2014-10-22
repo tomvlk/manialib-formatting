fc2d954d10aeda2526672037bbb8ab2c2545abdf
philippe <philippe@nadeo.com>
2014-10-22 13:39:41 +0200

    Deleted documentation for better readability

--- src/ManiaLib/Formatting/StringInterface.php
+++ src/ManiaLib/Formatting/StringInterface.php
@@ -7,48 +7,39 @@ interface StringInterface
     public function __construct($string);
 
     /**
-     * Remove codes provided from input string
      * @param string $codes
      * @return static
      */
     public function strip($codes);
 
     /**
-     * Remove all styles, colors and links from input string
-     * @param string[] $codes
+     * @param string $codes
      * @return static
      */
     public function stripAll();
 
     /**
-     * Remove every links from input string
      * @return static
      */
     public function stripLinks();
 
     /**
-     * Remove every colors code from input string
-     * Remove
      * @return static
      */
     public function stripColors();
 
     /**
-     * Replace escaped characters by their value
-     * i.e: $$ will be replaced by $
      * @return static
      */
     public function stripEscapeCharacter();
 
     /**
-     * Change string colors to increase contrast with backgroundColor
-     * @param string $backgroundColor chaine au format RGB12 ou RGB24, i.e FFF ou FFFFFF
+     * @param string $backgroundColor
      * @return static
      */
     public function contrastColors($backgroundColor);
 
     /**
-     * Return the modified string
      * @return string
      */
     public function __toString();
