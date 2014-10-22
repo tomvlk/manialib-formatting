60d4d40b271522a62f5588d413d48ccfd979dfca
philippe <philippe@nadeo.com>
2014-10-22 10:32:02 +0200

    Changed constants value

--- src/ManiaLib/Formatting/Lexer.php
+++ src/ManiaLib/Formatting/Lexer.php
@@ -9,25 +9,25 @@ class Lexer extends \Doctrine\Common\Lexer\AbstractLexer
     // Escaped charaters
     const T_ESCAPED_CHAR = 1;
     //Modifiers
-    const T_COLOR = 4;
-    const T_NO_COLOR = 5;
-    const T_SHADOWED = 6;
-    const T_BOLD = 7;
-    const T_ITALIC = 8;
-    const T_WIDE = 9;
-    const T_NARROW = 10;
-    const T_MEDIUM = 11;
-    const T_UPPERCASE = 12;
-    const T_RESET_ALL = 13;
-    const T_PUSH = 14;
-    const T_POP = 15;
+    const T_COLOR = 2;
+    const T_NO_COLOR = 3;
+    const T_SHADOWED = 4;
+    const T_BOLD = 5;
+    const T_ITALIC = 6;
+    const T_WIDE = 7;
+    const T_NARROW = 8;
+    const T_MEDIUM = 9;
+    const T_UPPERCASE = 10;
+    const T_RESET_ALL = 11;
+    const T_PUSH = 12;
+    const T_POP = 13;
     //Links
-    const T_EXTERNAL_LINK = 11;
-    const T_EXTERNAL_HIDDEN_LINK = 12;
-    const T_INTERNAL_LINK = 13;
-    const T_INTERNAL_HIDDEN_LINK = 14;
+    const T_EXTERNAL_LINK = 14;
+    const T_EXTERNAL_HIDDEN_LINK = 15;
+    const T_INTERNAL_LINK = 16;
+    const T_INTERNAL_HIDDEN_LINK = 17;
     //Other markups
-    const T_UNKNOWN_MARKUP = 20;
+    const T_UNKNOWN_MARKUP = 18;
 
     protected function getCatchablePatterns()
     {
